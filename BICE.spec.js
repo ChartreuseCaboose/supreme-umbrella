bcc061ea8b9fb0f9581d99acf8c18b8f1d5b212c
ChartreuseCaboose <Jeffmosley42@gmail.com>
2023-07-01 21:22:06 -0500

    implement damagePhase and some of firePhase, play with testing

--- BICE.spec.js
+++ BICE.spec.js
@@ -1,10 +1,42 @@
 const hour = require('./BICE/files/hour.js');
-describe ('damage methods', function() {
-    test ('Inflicts strength damage per hit.', function() {
+describe ('damage methods', function(){
+    test ('Inflicts strength damage per hit.', function(){
         expect(hour.damageToStrength(10)).toBe(22.5);
     });
-    test ('Inflicts organization damage per hit.', function() {
-        expect(hour.damageToOrg(10)).toBe(2);
+    test ('Inflicts organization damage per hit.', function(){
+        expect(hour.damageToOrg(10)).toBeCloseTo(2);
+    });
+//Restructure means this no longer tests cleanup, test again later
+    /*    test ('cleanup function', function(){
+        hour.damagePhase(10),
+        hour.cleanup(),
+        hour.damagePhase(5)
+        expect(hour.orgLoss).toBe(1);
+    });
+*/
+});
+
+describe ('damagePhase', function(){
+    test('damagePhase correctly calls damageToStrength and DamageToOrg', function(){
+        hour.damagePhase(10),
+        expect(hour.strengthLoss).toBe(22.5);
+        expect(hour.orgLoss).toBeCloseTo(2);
     });
 });
 
+describe ('firePhase', function(){
+    test('no defense points', function(){
+        hour.firePhase(100, 0),
+        expect(hour.firePhase(100)).toBe(52);
+    });
+    test('shots exceed defense points', function(){
+        expect(hour.firePhase(200, 100)).toBeCloseTo(82);
+    });
+    test('defense points exceed shots', function(){
+        expect(hour.firePhase(100, 200)).toBeCloseTo(30);
+    });
+    test('firePhase and damagePhase work together', function(){
+        hour.firePhase(100, 0),
+        expect(hour.strengthLoss).toBe(52*2.25);
+    });
+});
--- BICE/files/hour.js
+++ BICE/files/hour.js
@@ -1,24 +1,56 @@
 const hour = {
+    hits: 0,
     strengthLoss: 0,
     orgLoss: 0,
-    
-    damageToStrength(hits){
-    //  damage roll replaced with average during testing
-    for (i=0; i<hits; i++){
-        this.strengthLoss += 2.25;
-    };
-        this.strengthLoss = Number(this.strengthLoss.toFixed(2));
-        return this.strengthLoss
+
+    firePhase(shots, defensePoints){
+        let hits = 0;
+        let defense = defensePoints;
+        for (i=0; i<shots; i++){
+            if (defense > 0){
+                defense--;
+                //hits roll replaced with average during testing
+                hits += .3;
+            }
+        else hits +=.52;
+        };
+        hits = Number(hits.toFixed());
+        this.damagePhase(hits);
+        return hits
+    },
+        softnessCheck(softness){
+            let soft = true;
+            //rng heresy detected
+            //if (softness < (Math.floor(Math.random()*100)) < softness); soft = false;
+            return soft
+        },
+
+    damagePhase(hits){
+        this.strengthLoss = this.damageToStrength(hits);
+        this.orgLoss = this.damageToOrg(hits);
     },
 
-    damageToOrg(hits){
-     //damage roll replaced with average during testing
-    for (i=0; i<hits; i++) {
-        this.orgLoss += 0.2;
-    };
-        this.orgLoss = Number(this.orgLoss.toFixed(2));
-        return this.orgLoss
-    },
+        damageToStrength(hits){
+        //  damage roll replaced with average during testing
+        let damage = 0;
+        for (i=0; i<hits; i++){
+            //damage = damage + ((Math.floor((Math.random()*2))+1)*1.5);
+            damage += 2.25;
+        };
+            damage = Number(damage.toFixed(2));
+            return damage
+        },
+
+        damageToOrg(hits){
+        //damage roll replaced with average during testing
+        let damage = 0;
+        for (i=0; i<hits; i++) {
+        //damage = damage + (Math.floor((Math.random()*3)+1)*.1);
+            damage += 0.2;
+        };
+            damage = Number(damage.toFixed(3));
+            return damage
+        },
 
     cleanup(){
         this.strengthLoss = 0;
